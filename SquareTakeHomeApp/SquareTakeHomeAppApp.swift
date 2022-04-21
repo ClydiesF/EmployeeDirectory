95cc677cbd9e53b08a6e246014ef7bc29c941fd8
Clyde Freeman <c.edward.freeman@gmail.com>
2022-04-20 20:54:44 -0500

    Initial Commit

---
+++ SquareTakeHomeApp/SquareTakeHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareTakeHomeAppApp.swift
+//  SquareTakeHomeApp
+//
+//  Created by clydies freeman on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SquareTakeHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
